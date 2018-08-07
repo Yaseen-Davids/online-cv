320838ac9162be5eaa867989a2de66f236801031
Yaseen Davids <yaseendavids477@gmail.com>
2018-08-07 23:05:10 +0200

    comments added

--- js/script.js
+++ js/script.js
@@ -18,31 +18,39 @@ $(document).ready(function(){
 
     });
 
-    let aboutPos = document.getElementById("about-section").offsetTop - 200;
-    let theStudPos = document.getElementById("stud-section").offsetTop - 300;
-    let theExpPos = document.getElementById("exp-section").offsetTop - 300;
-    let theContPos = document.getElementById("contact-section").offsetTop - 300;
+    let aboutPos = document.getElementById("about-section").offsetTop - 150;
+    let theStudPos = document.getElementById("stud-section").offsetTop - 150;
+    let theExpPos = document.getElementById("exp-section").offsetTop - 150;
+    let theProjPos = document.getElementById("project-section").offsetTop - 150;
+    let theContPos = document.getElementById("contact-section").offsetTop - 150;
 
+    // if user clicks button smooth scroll to div
     $("#goToAbout").on('click', function(){
         $('html, body').animate({scrollTop: aboutPos}, 1500);
     });
 
+    // if user clicks button smooth scroll to div
     $("#goToStud").on('click', function(){
         $('html, body').animate({scrollTop: theStudPos}, 1500);
     });
 
+    // if user clicks button smooth scroll to div
+    $("#goToProj").on('click', function(){
+        $('html, body').animate({scrollTop: theProjPos}, 1500);
+    });
+
+    // if user clicks button smooth scroll to div
     $("#goToExp").on('click', function(){
         $('html, body').animate({scrollTop: theExpPos}, 1500);
     });
 
+    // if user clicks button smooth scroll to div
     $("#goToCont").on('click', function(){
         $('html, body').animate({scrollTop: theContPos}, 1500);
     });
 
-    $("#contact-me-logo").on('click', function(){
-        $("#contact-me-main").fadeToggle('slow', 'linear');
-    })
 
+    // if user clicks hamburger button, show navmenu
     $(".hamburger-btn").on('click', function(){
 
         if ($(".hamburger-menu").css('height') == "50px"){
@@ -62,33 +70,39 @@ $(document).ready(function(){
 
     onscroll = function(){
 
-        let studPos = document.getElementById("stud-section").offsetTop - 500;
-        let expPos = document.getElementById("exp-section").offsetTop - 400;
-        let hobPos = document.getElementById("hob-section").offsetTop - 500;
+        let studPos = document.getElementById("stud-section").offsetTop - 600;
+        let expPos = document.getElementById("exp-section").offsetTop - 600;
+        let hobPos = document.getElementById("hob-section").offsetTop - 600;
+        let projPos = document.getElementById("project-section").offsetTop - 600;
         
-        if (window.scrollY >= 500){
+        // If user scroll greater than 400
+        if (window.scrollY >= 400){
             $("#about-section").css('opacity', '1');
             $("#about-section").css('margin-top', '20px');
-            $("#contact-me-logo").css('right', '10px');
         }
 
+        // If user scroll greater than studies section
         if (window.scrollY >= studPos){
             $("#stud-section").css('opacity', '1');
             $("#stud-section").css('margin-top', '20px');
         }
 
+        // If user scroll greater than experience section
         if (window.scrollY >= expPos){
             $("#exp-section").css('opacity', '1');
             $("#exp-section").css('margin-top', '20px');
         }
 
+        // If user scroll greater than projects section
+        if (window.scrollY >= projPos){
+            $("#projects-main").css('opacity', '1');
+            $("#projects-main").css('margin-top', '20px');
+        }
+
+        // If user scroll greater than hobbies section
         if (window.scrollY >= hobPos) {
             $("#hob-section").css('opacity', '1');
             $("#hob-section").css('margin-top', '20px');
-        }
-
-        else if (window.scrollY < 500){
-            $("#contact-me-logo").css('right', '-100px');
         }
     }
 })
